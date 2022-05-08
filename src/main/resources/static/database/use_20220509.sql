b5916921d5cca838920ad6e912f78e6b97ae6b77
HT-Gilbert <hyunseokyong@gmail.com>
2022-05-09 00:25:21 +0900

    sql추가

---
+++ src/main/resources/static/database/use_20220509.sql
@@ -0,0 +1,6 @@
+INSERT INTO helpdesk.`user` (userid,username,userrole,totallogincount,loginfailcount,password,lastlogintime,registertime,updatetime,changepwdupdatedate,enckey,encpwd) VALUES
+	 ('admin','관리자','ROLE_ADMIN',NULL,NULL,'$2a$10$jGr7426aF8Zj8IDRlzwPdOUg2MR1fUEvkP9A.PU/A0.A.wgkM.V3K','2022-05-08 18:31:53.0',NULL,NULL,NULL,'1','1'),
+	 ('helpadmin','부관리자','ROLE_ADMIN',NULL,NULL,'$2a$10$M6OSsLNuZQWpRrFVwS.Xpe7a/.VHk.mXxEQspqUHKvYXr5ytg39Zi','2022-05-08 00:40:37.0',NULL,NULL,NULL,'1','$2a$10$SSeXDLjYSPtmtzn3QyFfoO9Vlkq2juf65/LQqT5D/UBy4Heyb5FSm
+'),
+	 ('user','일반멤버','ROLE_USER',NULL,NULL,'$2a$10$SSeXDLjYSPtmtzn3QyFfoO9Vlkq2juf65/LQqT5D/UBy4Heyb5FSm','2022-05-08 23:32:43.0',NULL,NULL,NULL,'1','$2a$10$SSeXDLjYSPtmtzn3QyFfoO9Vlkq2juf65/LQqT5D/UBy4Heyb5FSm
+');
